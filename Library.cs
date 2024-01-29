cc26b581d860af562125980e87e741603de24154
Domanstik <marinovich.daniil@gmail.com>
2024-01-29 19:37:34 +0300

    Class Library

---
+++ Library.cs
@@ -0,0 +1,13 @@
+﻿namespace Library
+{
+    public class Library
+    {
+        public int ISBN { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public DateTime TakeTime { get; set; }
+        public DateTime ReturnTime { get; set; }
+    }
+}
